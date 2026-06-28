711ca8e48294be47f4fd3cd414c15fd153da0abc
Adriano Abner Da Silva Bezerra <adriano.abner@live.com>
2026-06-27 23:14:56 -0300

    refactring: function GNL for norminette

--- get_next_line.c
+++ get_next_line.c
@@ -6,30 +6,41 @@
 /*   By: adrianda <adrianda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/27 17:24:33 by adrianda          #+#    #+#             */
-/*   Updated: 2026/06/27 19:52:12 by adrianda         ###   ########.fr       */
+/*   Updated: 2026/06/27 22:16:09 by adrianda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+static char	*ft_read_buff(int fd, char *remainder)
+{
+	ssize_t		bytes;
+	char		buf[BUFFER_SIZE + 1];
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	while (ft_strchr(remainder, '\n') == NULL)
+	{
+		bytes = read(fd, buf, sizeof(buf) - 1);
+		if (bytes <= 0)
+			break ;
+		buf[bytes] = '\0';
+		remainder = ft_strjoin(remainder, buf);
+	}
+	return (remainder);
+}
 
 char	*get_next_line(int fd)
 {
 	static char	*remainder;
 	char		*line;
 	char		*tmp;
-	ssize_t		bytes;
-	char		buf[BUFFER_SIZE + 1];
 	int			i;
 
 	i = 0;
-	bytes = read(fd, buf, sizeof(buf) - 1);
-	while (bytes > 0 && ft_strchr(remainder, '\n') == NULL)
-	{
-		buf[bytes] = '\0';
-		remainder = ft_strjoin(remainder, buf);
-		bytes = read(fd, buf, sizeof(buf) - 1);
-		buf[bytes] = '\0';
-	}
+	remainder = ft_read_buff(fd, remainder);
+	if (remainder == NULL || !*remainder)
+		return (NULL);
 	while (remainder[i] != '\0' && remainder[i] != '\n')
 		i++;
 	tmp = remainder;
